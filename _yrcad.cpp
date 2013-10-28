5f0b4f1cb772b5f0d68f1efd67ade504098313c2
Y. Sapir <yasapir@gmail.com>
2013-10-28 03:36:53 +0200

    Wrap BRepPrimAPI_MakeBox with Box class in _yrcad extension.

--- _yrcad.cpp
+++ _yrcad.cpp
@@ -1,10 +1,46 @@
-#include "rice/Class.hpp"
+#include <BRepBuilderAPI_MakeShape.hxx>
+#include <BRepPrimAPI_MakeBox.hxx>
+#include <rice/Class.hpp>
 
 using namespace Rice;
 
 
+static Data_Object<TopoDS_Shape> wrapShape(const TopoDS_Shape &shape)
+{
+    return Data_Object<TopoDS_Shape>(new TopoDS_Shape(shape));
+}
+
+static Data_Object<TopoDS_Shape> wrapShapeFromMaker(
+    const BRepBuilderAPI_MakeShape &maker)
+{
+    return wrapShape(maker.Shape());
+}
+
+
+void box_initialize(Object self, double xsize, double ysize, double zsize)
+{
+    self.iv_set("@xsize", xsize);
+    self.iv_set("@ysize", ysize);
+    self.iv_set("@zsize", zsize);
+
+    self.iv_set("@shape",
+        wrapShapeFromMaker(BRepPrimAPI_MakeBox(xsize, ysize, zsize)));
+}
+
+void union_initialize(Object self, Object a, Object b)
+{
+    self.iv_set("@a", a);
+    self.iv_set("@b", b);
+}
+
 extern "C"
 void Init__yrcad()
 {
-    Class rb_cYumyum = define_class("Yumyum");
+    Class rb_cShape = define_class("Shape");
+
+    Class rb_cBox = define_class("Box")
+        .define_method("initialize", &box_initialize);
+
+    Class rb_cUnion = define_class("Union")
+        .define_method("initialize", &union_initialize);
 }
--- extconf.rb
+++ extconf.rb
@@ -1,2 +1,8 @@
 require 'mkmf-rice'
+
+OCE_INCLUDE_DIR = '/usr/local/include/oce'
+OCE_LIB_DIR = '/usr/local/lib'
+
+dir_config('Prim', OCE_INCLUDE_DIR, OCE_LIB_DIR)
+
 create_makefile('_yrcad')
