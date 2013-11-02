51f95b46d65a45148463570f5f1e5bc56aebd665
Y. Sapir <yasapir@gmail.com>
2013-11-02 23:43:31 +0200

    Clean up pedestal.rb example.

--- pedestal.rb
+++ pedestal.rb
@@ -1,13 +1,13 @@
-require_relative 'rcad'
+require 'rcad'
 
 
 base = box(10.cm, 10.cm, 3.cm)
     .move(-5.cm, -5.cm, 0)
 
-pedestal = base
-    .add(cylinder(8.cm, 7.cm).move_z(3.cm))
-    .add(base.move_z(3.cm + 7.cm))
+pedestal = add do
+  ~base
+  ~cylinder(8.cm, 7.cm).move_z(3.cm)
+  ~base.move_z(3.cm + 7.cm)
+end
 
-pedestal
-  .rot_z(45)
-  .write_stl("pedestal.stl")
+~pedestal.rot_z(45)
